04f82a248023a5d975811fe60e982d511af06319
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2020-08-02 14:29:06 -0700

    Added functionality of updating existing table

--- src/main.py
+++ src/main.py
@@ -1,54 +1,63 @@
 #!/usr/bin/python
 import sys
+import os
 
-contestTableStructure = 'Spis Treści \n=================\n\n<!--ts-->\n\n<!--te-->\n'
+tagsForHeader = ('<h1>', '</h1>')
 
-def findAllOccurences(a_str, sub):
+def findAllOccurences(string, subString):
     start = 0
     indexes = []
     while 1:
-        start = a_str.find(sub, start)
+        start = string.find(subString, start)
         if start == -1: 
         	return indexes
-        indexes.append(start + len(sub))
-        start += len(sub)
+        indexes.append(start + len(subString))
+        start += len(subString)
 
-def generateTableOfContensts(fileName):
+def generateTableOfContensts(fileName, tableOfContentsName):
 	with open(fileName) as myFile:
 		data = myFile.read()
 
-	if data.find("<!--ts-->") >= 0:
-		print('Table of contents is already there')
+	index = data.find(tagsForHeader[0]) if data.find(tagsForHeader[0]) >= 0 else 0
+
+	if data.find('<!--ts-->') >= 0:
+		data = data[:data.find('<!--ts-->') + len('<!--ts-->')] + data[data.find('<!--te-->'):]
 
 	else:
-		index = data.find("<h1>")
-
-		if index >= 0:
+		contestTableStructure = tableOfContentsName + '\n=================\n\n<!--ts-->\n\n<!--te-->\n\n'
 		
-			data = data[:index] + contestTableStructure + data[index:]
+		data = data[:index] + contestTableStructure + data[index:]
 
-			index = data.find('<!--ts-->') + len('<!--ts-->')
-			
-			headers = []
-			
-			for i in findAllOccurences(data, '<h1>'):
-			
-				header = ''
-				j = 0
-				while data[i + j: i + j + len('</h1>')] != '</h1>' and i + j < len(data):
-					header += data[i+j]
-					j += 1
-					
-				headers.append(header)
+	index = data.find('<!--ts-->') + len('<!--ts-->')
+	
+	headers = []
+	
+	for i in findAllOccurences(data, tagsForHeader[0]):
+	
+		header = ''
+		j = 0
+		while data[i + j: i + j + len(tagsForHeader[1])] != tagsForHeader[1] and i + j < len(data):
+			header += data[i+j]
+			j += 1
 		
-			for header in headers:
-				line = '\n\t* [' + header + '](#' + header.replace(' ', '-') + ')' 
-				data = data[:index] + line + data[index:]	
-				index += len(line)
-				
-			with open("README.md", "w") as myFile:
-				myFile.write(data)
+		header.replace(',', '|')
+		headers.append(header)
+
+	for header in headers:
+		line = '\n   * [' + header + '](#' + header.replace(' ', '-') + ')' 
+		data = data[:index] + line + data[index:]	
+		index += len(line)
+		
+	with open('README.md', 'w') as myFile:
+		myFile.write(data)
 			
-if __name__ == "__main__":
-	fileName = str(sys.argv[1]) if len(sys.argv) > 1 else "README.md"
-	generateTableOfContensts(fileName)
+if __name__ == '__main__':
+	
+	fileName = str(sys.argv[1]) if len(sys.argv) > 1 else 'README.md'
+	tableOfContentsName = str(sys.argv[2]) if len(sys.argv) > 2 else 'Table of Contents'
+	
+	if (os.path.isfile(fileName)):
+		generateTableOfContensts(fileName, tableOfContentsName)
+	
+	else:
+		print('File ' + fileName + ' does not exist!')
