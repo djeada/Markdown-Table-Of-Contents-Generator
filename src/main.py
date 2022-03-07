5db306ab00e85fc66bf963c63885e7d8cc1aa5d0
djeada <addjellouli1@gmail.com>
2022-03-07 21:32:22 +0100

    [fix]: correct find_headers_markdown function and unit test

--- src/main.py
+++ src/main.py
@@ -67,12 +67,11 @@ def find_headers_markdown(file_contents: str, depth: int = 1) -> List[str]:
     if depth < 1 or depth > 6:
         raise ValueError('Depth must be between 1 and 6')
 
-    headers = []
-    # get the whole line after the header #
-    # add it to the list of headers
-    for i in range(file_contents.find('#' * depth), len(file_contents)):
-        if file_contents[i] == '\n':
-            headers.append(file_contents[file_contents.find('#' * depth) + depth:i])
+    headers = [line for line in file_contents.split('\n') if line.lstrip().startswith(f'#'*depth) and not line.lstrip().startswith(f'#'*(depth+1)) ]
+    # filter out '#'*depth from lines
+    headers = [header.replace(f'#'*depth, '') for header in headers]
+    # strip whitespaces
+    headers = [header.strip() for header in headers]
 
     return headers
 
@@ -135,7 +134,7 @@ def remove_table_of_contents(file_contents: str) -> str:
 
 def main():
     config = parse_args()
-    file_contents = open(config.file_name, 'r').read()
+    file_contents = open(config.file_name, 'r',  encoding="utf-8").read()
 
     if is_table_of_contents_present(file_contents):
         file_contents = remove_table_of_contents(file_contents)
@@ -143,7 +142,7 @@ def main():
     table_of_contents = generate_table_of_contents(file_contents, config.table_of_contents_name)
     # put it on top of the file
     file_contents = table_of_contents + '\n\n' + file_contents.lstrip()
-    open(config.file_name, 'w').write(file_contents)
+    open(config.file_name, 'w',  encoding="utf-8").write(file_contents)
 
 
 if __name__ == '__main__':
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
-from src.main import is_using_html_tags, is_using_markdown_syntax, find_headers_html
+from src.main import is_using_html_tags, is_using_markdown_syntax, find_headers_html, find_headers_markdown
 
-html_file_contensts = '''
+html_file_contents = '''
 <h1>Title</h1>
 <p>This is a paragraph.</p>
 <h2>Subtitle</h2>
@@ -19,19 +19,50 @@ html_file_contensts = '''
 <p>This is a paragraph.</p>
 '''
 
+markdown_file_contents = '''
+# Title
+This is a paragraph.
+## Subtitle
+This is a paragraph.
+# Programs
+## Python
+This is a paragraph.
+## Java
+This is a paragraph.
+## C++
+This is a paragraph.
+# Languages
+## English
+This is a paragraph.
+## Spanish
+This is a paragraph.
+'''
+
+
 def test_is_using_html_tags():
-    assert is_using_html_tags(html_file_contensts)
-    assert not is_using_markdown_syntax(html_file_contensts)
+    assert is_using_html_tags(html_file_contents)
+    assert not is_using_markdown_syntax(html_file_contents)
+
 
 def test_find_headers_html():
-
-    depth_to_headers = {1 : ['Title', 'Programs', 'Languages'],
-                        2 : ['Python', 'Java', 'C++', 'English', 'Spanish', 'Subtitle'],
-                        3 : list()
+    depth_to_headers = {1: ['Title', 'Programs', 'Languages'],
+                        2: ['Python', 'Java', 'C++', 'English', 'Spanish', 'Subtitle'],
+                        3: list()
                         }
 
     for depth, headers in depth_to_headers.items():
         expected = sorted(headers)
-        actual = sorted(find_headers_html(html_file_contensts, depth))
+        actual = sorted(find_headers_html(html_file_contents, depth))
         assert expected == actual
 
+
+def test_find_headers_markdown():
+    depth_to_headers = {1: ['Title', 'Programs', 'Languages'],
+                        2: ['Python', 'Java', 'C++', 'English', 'Spanish', 'Subtitle'],
+                        3: list()
+                        }
+
+    for depth, headers in depth_to_headers.items():
+        expected = sorted(headers)
+        actual = sorted(find_headers_markdown(markdown_file_contents, depth))
+        assert expected == actual
