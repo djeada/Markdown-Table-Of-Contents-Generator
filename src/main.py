43bbc1ea1dacf188674bcadfa64251cbc96a777e
djeada <addjellouli1@gmail.com>
2022-03-07 22:13:36 +0100

    [formatting] unifying formatting

--- src/main.py
+++ src/main.py
@@ -5,23 +5,28 @@ import argparse
 
 @dataclass
 class Config:
-    file_name: str = 'README.md'
-    output_file_name: str = 'README.md'
-    table_of_contents_name: str = 'Table of Contents'
+    file_name: str = "README.md"
+    output_file_name: str = "README.md"
+    table_of_contents_name: str = "Table of Contents"
     depth: int = 2
 
 
 @dataclass
 class Tags:
-    TABLE_CONTENTS_START: str = '<!--ts-->'
-    TABLE_CONTENTS_END: str = '<!--te-->'
+    TABLE_CONTENTS_START: str = "<!--ts-->"
+    TABLE_CONTENTS_END: str = "<!--te-->"
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Generate table of contents for README.md file.')
-    parser.add_argument('-i', '--input', help='input file name', required=False)
-    parser.add_argument('-o', '--output', help='output file name', required=False)
-    parser.add_argument('-d', '--depth', help='depth of headers', required=False)
-    parser.add_argument('-t', '--table-of-contents', help='table of contents name', required=False)
+    parser = argparse.ArgumentParser(
+        description="Generate table of contents for README.md file."
+    )
+    parser.add_argument("-i", "--input", help="input file name", required=False)
+    parser.add_argument("-o", "--output", help="output file name", required=False)
+    parser.add_argument("-d", "--depth", help="depth of headers", required=False)
+    parser.add_argument(
+        "-t", "--table-of-contents", help="table of contents name", required=False
+    )
 
     args = parser.parse_args()
     config = Config()
@@ -38,60 +43,70 @@ def parse_args():
 
 
 def is_using_html_tags(file_contents: str) -> bool:
-    '''
+    """
     Check if file is using html tags for declaration of headers.
-    '''
-    html_tags = (f'<h{i}>' for i in range(1, 6))
+    """
+    html_tags = (f"<h{i}>" for i in range(1, 6))
     return any(tag in file_contents for tag in html_tags)
 
 
 def is_using_markdown_syntax(file_contents: str) -> bool:
-    '''
+    """
     Check if file is using markdown syntax for headers.
-    '''
-    return '#' in file_contents
+    """
+    return "#" in file_contents
 
 
 def find_headers_html(file_contents: str, depth: int = 1) -> List[str]:
-    '''
+    """
     Find headers in file.
-    '''
+    """
     if depth < 1 or depth > 6:
-        raise ValueError('Depth must be between 1 and 6')
+        raise ValueError("Depth must be between 1 and 6")
 
-    headers = [line for line in file_contents.split('\n') if f'<h{depth}>' in line]
+    headers = [line for line in file_contents.split("\n") if f"<h{depth}>" in line]
     # filter out <h{depth}> </h{depth}> from lines
-    headers = [header.replace(f'<h{depth}>', '').replace(f'</h{depth}>', '') for header in headers]
+    headers = [
+        header.replace(f"<h{depth}>", "").replace(f"</h{depth}>", "")
+        for header in headers
+    ]
     # strip whitespaces
     headers = [header.strip() for header in headers]
     # replace spaces with -
-    headers = [header.replace(' ', '-') for header in headers]
+    headers = [header.replace(" ", "-") for header in headers]
 
     return headers
 
 
 def find_headers_markdown(file_contents: str, depth: int = 1) -> List[str]:
-    '''
+    """
     Find headers in file.
-    '''
+    """
     if depth < 1 or depth > 6:
-        raise ValueError('Depth must be between 1 and 6')
+        raise ValueError("Depth must be between 1 and 6")
 
-    headers = [line for line in file_contents.split('\n') if line.lstrip().startswith(f'#'*depth) and not line.lstrip().startswith(f'#'*(depth+1)) ]
+    headers = [
+        line
+        for line in file_contents.split("\n")
+        if line.lstrip().startswith(f"#" * depth)
+        and not line.lstrip().startswith(f"#" * (depth + 1))
+    ]
     # filter out '#'*depth from lines
-    headers = [header.replace(f'#'*depth, '') for header in headers]
+    headers = [header.replace(f"#" * depth, "") for header in headers]
     # strip whitespaces
     headers = [header.strip() for header in headers]
     # replace spaces with -
-    headers = [header.replace(' ', '-') for header in headers]
+    headers = [header.replace(" ", "-") for header in headers]
 
     return headers
 
 
-def generate_table_of_contents(file_contents: str, table_of_contents_name: str, depth: int) -> str:
-    '''
+def generate_table_of_contents(
+    file_contents: str, table_of_contents_name: str, depth: int
+) -> str:
+    """
     Generate table of contents for file.
-    '''
+    """
     if is_using_html_tags(file_contents):
         headers = find_headers_html(file_contents, depth)
 
@@ -100,62 +115,69 @@ def generate_table_of_contents(file_contents: str, table_of_contents_name: str, 
 
     headers = [f'- [{header.replace("-", " ")}](#{header})' for header in headers]
 
-    table_of_contents: List[str] = [f'## {table_of_contents_name}', f'{Tags.TABLE_CONTENTS_START}\n'] + headers + [f'\n{Tags.TABLE_CONTENTS_END}']
+    table_of_contents: List[str] = [
+        f"## {table_of_contents_name}",
+        f"{Tags.TABLE_CONTENTS_START}\n",
+    ] + headers + [f"\n{Tags.TABLE_CONTENTS_END}"]
 
-    return '\n'.join(table_of_contents)
+    return "\n".join(table_of_contents)
 
 
 def is_table_of_contents_present(file_contents: str) -> bool:
-    '''
+    """
     Check if table of contents is present in file.
-    '''
-    return Tags.TABLE_CONTENTS_START in file_contents and Tags.TABLE_CONTENTS_END in file_contents
+    """
+    return (
+        Tags.TABLE_CONTENTS_START in file_contents
+        and Tags.TABLE_CONTENTS_END in file_contents
+    )
 
 
 def remove_table_of_contents(file_contents: str) -> str:
-    '''
+    """
     Remove table of contents from file.
-    '''
-    #convert file_contents from str to list of strings 
-    tmp = file_contents.split('\n')
-    #find the index of the first line with Tags.TABLE_CONTENTS_START
+    """
+    # convert file_contents from str to list of strings
+    tmp = file_contents.split("\n")
+    # find the index of the first line with Tags.TABLE_CONTENTS_START
     start = tmp.index(Tags.TABLE_CONTENTS_START)
 
     if start == -1:
-        raise ValueError('Tags.TABLE_CONTENTS_START not found')
+        raise ValueError("Tags.TABLE_CONTENTS_START not found")
 
     print(start)
-    #remove the line above the first line with Tags.TABLE_CONTENTS_START
-    tmp = tmp[:start-1] + tmp[start:]
-    #convert the list back to a string
-    file_contents = '\n'.join(tmp)
+    # remove the line above the first line with Tags.TABLE_CONTENTS_START
+    tmp = tmp[: start - 1] + tmp[start:]
+    # convert the list back to a string
+    file_contents = "\n".join(tmp)
 
     # remove everything between Tags.TABLE_CONTENTS_START and Tags.TABLE_CONTENTS_END
     start = file_contents.find(Tags.TABLE_CONTENTS_START)
     stop = file_contents.find(Tags.TABLE_CONTENTS_END) + len(Tags.TABLE_CONTENTS_END)
 
-    file_contents = file_contents.replace(
-        file_contents[start:stop], '')
+    file_contents = file_contents.replace(file_contents[start:stop], "")
 
     # check if there are empty lines after start
-    while file_contents[start] == '\n':
-        file_contents = file_contents[:start] + file_contents[start+1:]
+    while file_contents[start] == "\n":
+        file_contents = file_contents[:start] + file_contents[start + 1 :]
 
     return file_contents
 
 
 def main():
     config = parse_args()
-    file_contents = open(config.file_name, 'r',  encoding="utf-8").read()
+    file_contents = open(config.file_name, "r", encoding="utf-8").read()
 
     if is_table_of_contents_present(file_contents):
         file_contents = remove_table_of_contents(file_contents)
 
-    table_of_contents = generate_table_of_contents(file_contents, config.table_of_contents_name, config.depth)
+    table_of_contents = generate_table_of_contents(
+        file_contents, config.table_of_contents_name, config.depth
+    )
     # put it on top of the file
-    file_contents = table_of_contents + '\n\n' + file_contents.lstrip()
-    open(config.output_file_name, 'w',  encoding="utf-8").write(file_contents)
+    file_contents = table_of_contents + "\n\n" + file_contents.lstrip()
+    open(config.output_file_name, "w", encoding="utf-8").write(file_contents)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
-from src.main import is_using_html_tags, is_using_markdown_syntax, find_headers_html, find_headers_markdown
+from src.main import (
+    is_using_html_tags,
+    is_using_markdown_syntax,
+    find_headers_html,
+    find_headers_markdown,
+)
 
-html_file_contents = '''
+html_file_contents = """
 <h1>Title</h1>
 <p>This is a paragraph.</p>
 <h2>Subtitle</h2>
@@ -17,9 +22,9 @@ html_file_contents = '''
 <p>This is a paragraph.</p>
 <h2>Spanish</h2>
 <p>This is a paragraph.</p>
-'''
+"""
 
-markdown_file_contents = '''
+markdown_file_contents = """
 # Title
 This is a paragraph.
 ## Subtitle
@@ -36,7 +41,7 @@ This is a paragraph.
 This is a paragraph.
 ## Spanish
 This is a paragraph.
-'''
+"""
 
 
 def test_is_using_html_tags():
@@ -45,10 +50,11 @@ def test_is_using_html_tags():
 
 
 def test_find_headers_html():
-    depth_to_headers = {1: ['Title', 'Programs', 'Languages'],
-                        2: ['Python', 'Java', 'C++', 'English', 'Spanish', 'Subtitle'],
-                        3: list()
-                        }
+    depth_to_headers = {
+        1: ["Title", "Programs", "Languages"],
+        2: ["Python", "Java", "C++", "English", "Spanish", "Subtitle"],
+        3: list(),
+    }
 
     for depth, headers in depth_to_headers.items():
         expected = sorted(headers)
@@ -57,10 +63,11 @@ def test_find_headers_html():
 
 
 def test_find_headers_markdown():
-    depth_to_headers = {1: ['Title', 'Programs', 'Languages'],
-                        2: ['Python', 'Java', 'C++', 'English', 'Spanish', 'Subtitle'],
-                        3: list()
-                        }
+    depth_to_headers = {
+        1: ["Title", "Programs", "Languages"],
+        2: ["Python", "Java", "C++", "English", "Spanish", "Subtitle"],
+        3: list(),
+    }
 
     for depth, headers in depth_to_headers.items():
         expected = sorted(headers)
